50c15bbfbbb5a4a3c12043c2aa8ea34b18dec02d
Pavel dub <dubstr1@gmail.com>
2020-04-03 19:21:19 +0300

    first commit

---
+++ canvas-script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Definitions
+  let canvas = document.getElementById('paint-canvas')
+  let context = canvas.getContext('2d')
+  let boundings = canvas.getBoundingClientRect()
+})
